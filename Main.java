c9364c559c34d67c6e592afa1d12cdbd8ed29580
lukakharkhelauri <148798486+lukakharkhelauri@users.noreply.github.com>
2024-10-18 19:14:09 +0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,69 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //exercise 1
+//        int number = 36;
+//        int fahrenheit= number * 9/5 + 32; //formula
+//        System.out.println(fahrenheit);
+
+        //exercise 2
+//        int number = 36;
+//        int celsius= (number - 32) * 5/9; //formula
+//        System.out.println(celsius);
+
+        //exercise 3
+//          if (number % 400 == 0) {
+//            System.out.println(" ნაკიანი წელია");
+//        } else if (number % 100 == 0) {
+//            System.out.println("არარის ნაკიანი წელი");
+//        } else if (number % 4 == 0) {
+//            System.out.println("ნაკიანი წელია");
+//        } else {
+//            System.out.println("არარი ნაკიანი");
+//        }
+
+        //exercise 5
+//        int x = 10;
+//        double y = -10;
+//
+//        if (x > 0 && y > 0) {
+//            System.out.println("პირველ ნაწილშია");
+//        } else if (x < 0 && y > 0) {
+//            System.out.println("მეორე ნაწილშია");
+//        } else if (x < 0 && y < 0) {
+//            System.out.println("მესამე ნაწილშია");
+//        } else if (x > 0 && y < 0) {
+//            System.out.println("მეოთხე ნაწილშია");
+//        } else if (x == 0 && y != 0) {
+//            System.out.println("y-ღერძზეა");
+//        } else if (y == 0 && x != 0) {
+//            System.out.println("x-ღერძზეა");
+//            }
+
+        //exercise 4
+//        String operator = "+";
+//        String string1 = "10";
+//        String string2 = "5";
+//        int number1 = Integer.parseInt(string1);
+//        int number2 = Integer.parseInt(string2);
+//        int result = 10;
+//
+//        switch (operator) {
+//            case "+":
+//                result = number1 + number2;
+//                break;
+//            case "-":
+//                result = number1 - number2;
+//                break;
+//            case "*":
+//                result = number1 * number2;
+//                break;
+//            case "/":
+//                result = number1 / number2;
+//                break;
+//            default:
+//        }
+//
+//        System.out.println(result);
+    }
+}
